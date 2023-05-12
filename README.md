24405a82bbf92ed8ec2752ededa77280f86e8d93
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-05-12 19:16:51 +0300

    zsh improved a lot, added a shit ton of plugins, switched back to betterdiscord and disable the discord update popups

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 
 ### Packages needed
 ```
-paru -S rustup chezmoi hyprland-git xdg-desktop-portal-hyprland-git grim polkit-gnome ffmpeg swaybg ttf-font-awesome \
+paru -S rustup go chezmoi hyprland-git xdg-desktop-portal-hyprland-git grim polkit-gnome ffmpeg swaybg ttf-font-awesome \
 rofi-git pavucontrol qt5ct zsh wev wl-clipboard wf-recorder kwallet-pam cliphist jaq ripgrep btop moreutils \
 swaybg grimblast-git ffmpegthumbnailer playerctl dictd qtkeychain-qt6 flameshot-git batify2-git \
 noise-suppression-for-voice lf spotify-player libdisplay-info spotifywm-git \
-eww-wayland wlogout swaylock-effects-git sddm-git pamixer neofetch espanso-wayland-git webcord  plymouth\
+eww-wayland wlogout swaylock-effects-git sddm-git pamixer neofetch espanso-wayland-git discord betterdiscordctl-git dplymouth\
 papirus-icon-theme-git swayosd-git dunst wezterm swayidle geticons udiskie python-pywal cpupower-git sddm-theme-astronaut
 
 ### depreaceated 
--- dot_zsh/executable_plugins.zsh
+++ dot_zsh/executable_plugins.zsh
@@ -1,8 +1,41 @@
-# Using normal load works
-zi depth=1 lucid nocd for \
-    romkatv/powerlevel10k
+if [[ ! -f "$zi_home/bin/zi.zsh" ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}Z-SHELL%F{220} A Swiss Army Knife for Zsh (%F{33}z-shell/zi%F{220})…%f"
+    command mkdir -p "$zi_home" && command chmod g-rwX "$zi_home"
+    command git clone https://github.com/z-shell/zi.git "$zi_home/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
 
-# PLUGINS
+# Loading ZI
+source "${zi_home}/bin/zi.zsh"
+autoload -Uz _zi
+(( ${+_comps} )) && _comps[zi]=_zi
+
+# Annexes (extensions) for ZI. Adds ice modifiers:
+#   - patch-dl: Download and apply patches. ice: `dl` `patch`
+#   - bin-gem-node: Executable not on PATH. `fbin` ice
+zi light-mode for \
+    z-shell/z-a-patch-dl \
+    z-shell/z-a-bin-gem-node
+
+## Plugins
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=1"
+zi ice id-as="autosuggestions"; zi light zsh-users/zsh-autosuggestions
+
+# Completions
+zi ice wait lucid blockf id-as="zsh-completions"
+zi light zsh-users/zsh-completions
+
+zi snippet PZT::modules/completion
+zstyle ':completion:*' completer _complete _match _expand
+
+zi wait lucid light-mode depth=1 for \
+  pick="autopair.zsh" atload="autopair-init" hlissner/zsh-autopair \
+  pick="async.zsh" mafredri/zsh-async
+
+# Fuzzy Finder package (from Zsh-Packages/fzf) (ctrl + T)
+zi pack"default+keys" for fzf
+
 zi ice wait
 zi snippet OMZP::dirhistory
 
@@ -28,14 +61,8 @@ zi light o1001100/ssh-connect
 zi ice wait
 zi light MichaelAquilina/zsh-you-should-use
 
-export YSU_IGNORED_ALIASES=("ls" "ll" "cd.." "pdw")
+export YSU_IGNORED_ALIASES=("ls" "ll" "cd.." "pdw", "git")
 export YSU_HARDCORE=1
-
-# replaces zsh-you-should-use
-# zi ice wait
-# zi light djui/alias-tips
-# export ZSH_PLUGINS_ALIAS_TIPS_EXPAND=0
-
 
 # check that you are not in docker or a server avoid in headless installs
 if [ -z "$DISPLAY" ] && [ -z "$SSH_CLIENT" ]; then
@@ -45,9 +72,21 @@ else
   zi light MichaelAquilina/zsh-auto-notify
 fi
 
-# ZSH AUTOCOMPLETION
-zi ice wait
-zi light marlonrichert/zsh-autocomplete
+## History search
+# ctrl-r
+zi light-mode for \
+  z-shell/H-S-MW \
+  zsh-users/zsh-history-substring-search
+
+# ZI plugin for diff-so-fancy https://github.com/z-shell/zsh-diff-so-fancy
+zi ice wait lucid as'program' pick'bin/git-dsf'
+zi load z-shell/zsh-diff-so-fancy
+
+# Theming
+
+# Using normal load works
+zi depth=1 lucid nocd for \
+    romkatv/powerlevel10k
 
 # zi ice wait
 # zi light tom-doerr/zsh_codex
@@ -55,15 +94,20 @@ zi light marlonrichert/zsh-autocomplete
 # zi ice wait
 # zi light RobSis/zsh-completion-generator
 
-# autocompletion additions + fast-syntax-highlighting + zsh-autosuggestions
-zi wait lucid for \
-  atinit'ZI[COMPINIT_OPTS]=-C; zicompinit; zicdreplay' \
-    zsh-users/zsh-syntax-highlighting \
-  blockf \
-    zsh-users/zsh-completions \
-  atload"!_zsh_autosuggest_start" \
-    zsh-users/zsh-autosuggestions
+# # autocompletion additions + fast-syntax-highlighting + zsh-autosuggestions
+# zi wait lucid for \
+#   atinit'ZI[COMPINIT_OPTS]=-C; zicompinit; zicdreplay' \
+#     zsh-users/zsh-syntax-highlighting \
+#   blockf \
+#     zsh-users/zsh-completions \
+#   atload"!_zsh_autosuggest_start" \
+#     zsh-users/zsh-autosuggestions
 
-# loaf after syntax highlighting to prevent "= is not a valid identifier" error
+# Syntax highlight must be the last one
+zi wait lucid for id-as="fast-highlight" \
+  atinit"ZI[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+    z-shell/F-SY-H
+
+# load after syntax highlighting to prevent "= is not a valid identifier" error
 zi ice wait
 zi light arzzen/calc.plugin.zsh
