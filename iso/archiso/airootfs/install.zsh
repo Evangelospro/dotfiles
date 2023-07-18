842f9da88229f925b67862ac0753556705833edc
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-07-18 05:19:18 +0300

    forgot to refresh externals

--- iso/archiso/airootfs/install.zsh
+++ iso/archiso/airootfs/install.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 PYTHON_VERSION=$(python -V | cut -d " " -f 2 | cut -d "." -f 1,2)
-chezmoi init --apply Evangelospro
+chezmoi init --apply Evangelospro -R
 cp $HOME/.config/hypr/monitors_extend.conf $HOME/.config/hypr/monitors.conf
 sudo cp $HOME/.local/share/chezmoi/confs/etc/* /etc -r
 sudo cp $HOME/.local/share/chezmoi/confs/opt/* /opt -r
@@ -10,7 +10,7 @@ vsCodeWorkbench=/opt/visual-studio-code/resources/app/out/vs/code/electron-sandb
 sudo cp -r $HOME/.local/share/chezmoi/dot_local/private_share/icons/vscode $vsCodeWorkbench
 sudo cp $HOME/.local/share/chezmoi/confs/vscode.css $vsCodeWorkbench/vsc.css
 sudo rm /usr/lib/python$PYTHON_VERSION/EXTERNALLY-MANAGED
-pip install -r $HOME/.local/share/chezmoi/requirments.txt
+pip install -r $HOME/.local/share/chezmoi/requirements.txt
 sudo pacman-key --init
 sudo pacman-key --populate
 rate-mirrors --allow-root arch | sudo tee /etc/pacman.d/mirrorlist
