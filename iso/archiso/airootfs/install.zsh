4f19997a378d64d056178f0c9f53fb51dd9e4d2a
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-07-25 14:05:55 +0300

    init rust on install.zsh

--- iso/archiso/airootfs/install.zsh
+++ iso/archiso/airootfs/install.zsh
@@ -18,12 +18,13 @@ mkdir -p $HOME/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Vide
 vsCodeWorkbench=/opt/visual-studio-code/resources/app/out/vs/code/electron-sandbox/workbench
 sudo cp -r $HOME/.local/share/chezmoi/dot_local/private_share/icons/vscode $vsCodeWorkbench
 sudo cp $HOME/.local/share/chezmoi/confs/vscode.css $vsCodeWorkbench/vsc.css
-sudo rm /usr/lib/python$PYTHON_VERSION/EXTERNALLY-MANAGED
-pip install -r $HOME/.local/share/chezmoi/requirements.txt
 sudo pacman-key --init
 sudo pacman-key --populate
 rate-mirrors --allow-root arch | sudo tee /etc/pacman.d/mirrorlist
 # paru -Syyu
+sudo rm /usr/lib/python$PYTHON_VERSION/EXTERNALLY-MANAGED
+pip install -r $HOME/.local/share/chezmoi/requirements.txt
+rustup default stable
 # cd /home/evangelospro/.local/share/chezmoi/confs/grub-themes/CyberRe && sudo ./install.sh
 # Add a "#" to start of the line that has "hyprload.sh" in it in $HOME/.config/hypr/startup.conf
 bash $HOME/.config/hypr/plugins/update.sh
