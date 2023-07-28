193063163a407a6d2ae5c0862705de905c753c35
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-07-28 19:48:46 +0300

    fixed various things...

--- iso/archiso/airootfs/install.zsh
+++ iso/archiso/airootfs/install.zsh
@@ -1,10 +1,11 @@
 #!/bin/zsh
+chsh -s /usr/bin/zsh
 PYTHON_VERSION=$(python -V | cut -d " " -f 2 | cut -d "." -f 1,2)
 chezmoi init --apply Evangelospro -R
 function update(){
     # chaotic was taking over hence hyprland is on hold
-    paru -Syu aur/hyprland-git && git pull $HOME/.config/hypr/hyprland-repo && $HOME/.config/hypr/plugins/update.sh
     paru -Syu
+    paru -Syu aur/hyprland-git && git pull $HOME/.config/hypr/hyprland-repo && $HOME/.config/hypr/plugins/update.sh
     zi self-update
     zi update
     navi-update
@@ -25,6 +26,9 @@ rate-mirrors --allow-root arch | sudo tee /etc/pacman.d/mirrorlist
 sudo rm /usr/lib/python$PYTHON_VERSION/EXTERNALLY-MANAGED
 pip install -r $HOME/.local/share/chezmoi/requirements.txt
 rustup default stable
+# Lolcate database
+lolcate --create
+lolcate --update 2>/dev/null
 # cd /home/evangelospro/.local/share/chezmoi/confs/grub-themes/CyberRe && sudo ./install.sh
 # Add a "#" to start of the line that has "hyprload.sh" in it in $HOME/.config/hypr/startup.conf
 bash $HOME/.config/hypr/plugins/update.sh
