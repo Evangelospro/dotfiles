276543b66829460049af9b2b4ebea830b5dd99c7
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-08-01 21:15:54 +0300

    refactor build pipeline

---
+++ iso/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Build ISO and rename to match BUILD_DATE
+# bash aur.sh enable
+# bash build-no-cache.sh
+# ISO_PATH=$(find . -type f -name "*.iso")
+# ISO_NAME=$(basename $ISO_PATH)
+# ISO_PKGS=$(find . -type f -name "*.txt")
+
+cd isoOUT
+
+# # split the iso if larger than 2GB
+# split -b 1950M $ISO_PATH "${ISO_NAME%.*}.part"
+# \ls -lasih .
+# for part in $(\ls *.part*); do sha256sum $part > "${part}.sha256"; done
+# \ls -lasih .
+
+echo "packages" > packages.txt
+echo "part" > test.part1
+echo "hash" > test.part1.sha256
