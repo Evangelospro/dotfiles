9a82046717e053b1739dc5e801abaadbcdc01c05
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-08-07 20:42:41 +0300

    Update build.sh

--- iso/build.sh
+++ iso/build.sh
@@ -7,10 +7,7 @@ ISO_PATH=$(find ./isoOUT -type f -name "*.iso")
 ISO_NAME=$(basename $ISO_PATH)
 ISO_PKGS=$(find ./isoOUT -type f -name "*.txt")
 
-mkdir -p isoOUT
-cd isoOUT
-
-# # split the iso if larger than 2GB
+# split the iso if larger than 2GB
 split -b 1950M $ISO_PATH "${ISO_NAME%.*}.part"
 \ls -lasih .
 for part in $(\ls *.part*); do sha256sum $part > "${part}.sha256"; done
