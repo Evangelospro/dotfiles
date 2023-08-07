d6704c94c958aeb3516192256b897774a60f92c6
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-08-07 07:02:30 +0300

    Update profiledef.sh

--- iso/archiso/profiledef.sh
+++ iso/archiso/profiledef.sh
@@ -18,13 +18,8 @@ file_permissions=(
     ["/etc/shadow"]="0:0:400"
     ["/root"]="0:0:750"
     ["/root/.automated_script.sh"]="0:0:755"
-    ["/usr/local/bin/choose-mirror"]="0:0:755"
     ["/usr/local/bin/Installation_guide"]="0:0:755"
     ["/usr/local/bin/livecd-sound"]="0:0:755"
     ["/etc/polkit-1/rules.d"]="0:0:750"
     ["/etc/sudoers.d"]="0:0:750"
-    ["/usr/local/bin/alci-make-a-pure-arch"]="0:0:755"
-    ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
-    ["/usr/local/bin/get-nemesis-on-alci"]="0:0:755"
-    ["/usr/local/bin/fix-keys"]="0:0:755"
 )
