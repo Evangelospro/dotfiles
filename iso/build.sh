8f25267ede6772eda79fe7af25c20fe3a5e7e6a0
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-08-07 22:41:41 +0300

    Update build.sh

--- iso/build.sh
+++ iso/build.sh
@@ -3,18 +3,13 @@
 # Build ISO and rename to match BUILD_DATE
 bash aur.sh enable
 bash build-no-cache.sh
-ISO_PATH=$(find ./isoOUT -type f -name "*.iso")
+cd isoOUT
+ISO_PATH=$(find . -type f -name "*.iso")
 ISO_NAME=$(basename $ISO_PATH)
-ISO_PKGS=$(find ./isoOUT -type f -name "*.txt")
+ISO_PKGS=$(find . -type f -name "*.txt")
 
 # split the iso if larger than 2GB
 split -b 1950M $ISO_PATH "${ISO_NAME%.*}.part"
 \ls -lasih .
 for part in $(\ls *.part*); do sha256sum $part > "${part}.sha256"; done
 \ls -lasih .
-
-# echo "packages" > packages.txt
-# echo "part" > test.part1
-# echo "hash" > test.part1.sha256
-
-# /bin/bash
