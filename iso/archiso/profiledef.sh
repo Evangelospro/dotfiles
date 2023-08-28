562a517f2b854d8c4dee969a461c5e0bfa906fb5
Evangelos Lioudakis <elioudakis123@gmail.com>
2023-08-28 11:57:53 +0300

    Update profiledef.sh

--- iso/archiso/profiledef.sh
+++ iso/archiso/profiledef.sh
@@ -17,7 +17,6 @@ file_permissions=(
     ["/usr/local/bin/installer"]="0:0:755"
     ["/etc/shadow"]="0:0:400"
     ["/root"]="0:0:750"
-    ["/usr/local/bin/Installation_guide"]="0:0:755"
     ["/usr/local/bin/livecd-sound"]="0:0:755"
     ["/etc/polkit-1/rules.d"]="0:0:750"
     ["/etc/sudoers.d"]="0:0:750"
