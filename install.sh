d138888300ff19ee6cbe3e76c8ffa61d6d4dbc06
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-02-19 14:39:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 # Refresh everything
 sudo pacman -Syu --needed base-devel
 
-# Install git, paru, chezmoi and rebos
+# Install chaotic aur
+echo "Setting up chaotic aur"
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 if ! pacman -Qs git >/dev/null 2>&1; then
     sudo pacman -Sy --noconfirm git
 fi
