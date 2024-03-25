40377853245774509becad24bfdce997cc3b4b07
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-03-25 09:14:47 +0200

    add actual compiled cursors

--- dot_config/shell/executable_aliases.sh
+++ dot_config/shell/executable_aliases.sh
@@ -105,7 +105,8 @@ alias disable-aslr='sudo sysctl -w kernel.randomize_va_space=0'
 alias enable-aslr='sudo sysctl -w kernel.randomize_va_space=2'
 
 # Cleanups
-alias clean-cache="sudo /usr/bin/rm -rf $HOME/.cache/* && _evalcache_clear" # shows no mercy but it is cache right?
+alias clean-cache="/usr/bin/rm -rf $HOME/.cache/* && _evalcache_clear" # shows no mercy but it is cache right?
+alias clean-vscode-cache="/usr/bin/rm -rf $HOME/.config/Code*/Cache/* && /usr/bin/rm -rf $HOME/.config/Code*/CachedData/*"
 alias clean-docker='docker system prune -af --volumes --filter="label!=DO_NOT_PRUNE=true"'
 alias clean-packages='sudo pacman -Rns $(pacman -Qtdq) && sudo /usr/bin/rm -rf /var/cache/pacman/pkg/*'
 alias clean-font-cache="fc-cache -f -v"
