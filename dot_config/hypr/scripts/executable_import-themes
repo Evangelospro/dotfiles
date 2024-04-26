80d3d00455b9f660e821cea58fe3a3c58f9f7ed3
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-04-26 14:29:16 +0300

    change bash headers in all files

--- dot_config/hypr/scripts/executable_import-themes
+++ dot_config/hypr/scripts/executable_import-themes
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # usage: import-gsettings
 config="${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
--- dot_config/waybar/scripts/executable_stats.sh
+++ dot_config/waybar/scripts/executable_stats.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-battery(){
+battery() {
     BAT=$(\ls /sys/class/power_supply | grep BAT | head -n 1)
     if [ -z $BAT ]; then
         case $1 in
-            --percent) echo "0";;
-            --status) echo "No battery connected";;
+        --percent) echo "0" ;;
+        --status) echo "No battery connected" ;;
         esac
     else
         percent=$(\cat /sys/class/power_supply/${BAT}/capacity)
         status=$(\cat /sys/class/power_supply/${BAT}/status)
         case $1 in
-            --percent) echo $percent;;
-            --status) echo $status;;
+        --percent) echo $percent ;;
+        --status) echo $status ;;
         esac
     fi
 }
@@ -20,7 +20,7 @@ battery(){
 network() {
     status=$(nmcli g | grep -oE "disconnected")
 
-    if [ $status ] ; then
+    if [ $status ]; then
         icon="睊"
         essid=""
     else
@@ -29,8 +29,8 @@ network() {
     fi
 
     case $1 in
-        --essid) echo $essid;;
-        --icon) echo $icon;;
+    --essid) echo $essid ;;
+    --icon) echo $icon ;;
     esac
 }
 
@@ -38,7 +38,7 @@ bluetooth() {
     status=$(bluetoothctl show | grep -oE "Powered: yes")
     devices=$(bluetoothctl devices | grep Device | cut -d ' ' -f 2-)
 
-    if [ $status ] ; then
+    if [ $status ]; then
         icon=""
         text=""
     else
@@ -47,8 +47,8 @@ bluetooth() {
     fi
 
     case $1 in
-        --devices) echo $devices;;
-        --icon) echo $icon;;
+    --devices) echo $devices ;;
+    --icon) echo $icon ;;
     esac
 }
 
@@ -58,9 +58,9 @@ memory() {
     free=$(expr $total - $used)
 
     case $1 in
-        --total) echo $total;;
-        --used) echo $used;;
-        --free) echo $free;;
+    --total) echo $total ;;
+    --used) echo $used ;;
+    --free) echo $free ;;
     esac
 }
 
@@ -70,12 +70,12 @@ brightness() {
     percent=$(echo "scale=2; $current / $max * 100" | bc)
 
     case $1 in
-        --percent) echo $percent;;
+    --percent) echo $percent ;;
     esac
 
 }
 
-notifications(){
+notifications() {
     dndStatus=$(swaync-client -D)
     if [[ "$dndStatus" == "true" ]]; then
         icon=""
@@ -84,17 +84,17 @@ notifications(){
     fi
     notificationsNum=$(swaync-client -c)
     case $1 in
-        --icon) echo $icon;;
-        --num) echo $notificationsNum;;
-        --all) echo "$icon $notificationsNum";;
+    --icon) echo $icon ;;
+    --num) echo $notificationsNum ;;
+    --all) echo "$icon $notificationsNum" ;;
     esac
 }
 
 sound() {
-    status=`amixer get Master | tail -n1 | grep -wo 'on'`
+    status=$(amixer get Master | tail -n1 | grep -wo 'on')
 
     if [[ "$status" == "on" ]]; then
-        volume=`amixer get Master | tail -n1 | awk -F ' ' '{print $5}' | tr -d '[%]'`
+        volume=$(amixer get Master | tail -n1 | awk -F ' ' '{print $5}' | tr -d '[%]')
     else
         volume=0
     fi
@@ -103,11 +103,11 @@ sound() {
     if [[ "$status" == "on" ]]; then
         if [[ "$current" -eq "0" ]]; then
             icon="ﱝ"
-            elif [[ ("$current" -ge "0") && ("$current" -le "30") ]]; then
+        elif [[ ("$current" -ge "0") && ("$current" -le "30") ]]; then
             icon="奄"
-            elif [[ ("$current" -ge "30") && ("$current" -le "60") ]]; then
+        elif [[ ("$current" -ge "30") && ("$current" -le "60") ]]; then
             icon="奔"
-            elif [[ ("$current" -ge "60") && ("$current" -le "100") ]]; then
+        elif [[ ("$current" -ge "60") && ("$current" -le "100") ]]; then
             icon="墳"
         fi
     else
@@ -115,17 +115,17 @@ sound() {
     fi
 
     case $1 in
-        --icon) echo $icon;;
-        --percent) echo $volume;;
+    --icon) echo $icon ;;
+    --percent) echo $volume ;;
     esac
 }
 
 # Check command-line arguments and call the appropriate function
 case $1 in
-    "battery") battery $2;;
-    "memory") memory $2;;
-    "network") network $2;;
-    "notifications") notifications $2;;
-    "sound") sound $2;;
-    "brightness") brightness $2;;
+"battery") battery $2 ;;
+"memory") memory $2 ;;
+"network") network $2 ;;
+"notifications") notifications $2 ;;
+"sound") sound $2 ;;
+"brightness") brightness $2 ;;
 esac
