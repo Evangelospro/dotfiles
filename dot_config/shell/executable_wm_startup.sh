8353ff82081d60ee16cdceec922d3b6a385cbe9f
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-05-17 17:57:17 +0300

    Backup commit

--- dot_config/shell/executable_wm_startup.sh
+++ dot_config/shell/executable_wm_startup.sh
@@ -1,4 +1,4 @@
 ## Launches WM on session start
-if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
+if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ] && ["$(stat -c %d:%i /)" = "$(stat -c %d:%i /proc/1/root.)"]; then
     exec $HOME/.config/hypr/start
 fi
