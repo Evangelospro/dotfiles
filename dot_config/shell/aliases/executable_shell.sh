71296376c1e25d66aa41edab8f61b32679e21575
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-05-19 17:50:38 +0300

    fixes to systemd services

--- dot_config/shell/aliases/executable_shell.sh
+++ dot_config/shell/aliases/executable_shell.sh
@@ -1,4 +1,4 @@
 # Shell
 alias reload-shell="exec $SHELL -l"
-alias tobash="sudo chsh evangelospro -s /bin/bash && echo 'Now log out.'"
-alias tozsh="sudo chsh evangelospro -s /bin/zsh && echo ''Now log out.'"
+alias tobash="chsh -s /bin/bash && echo 'Now log out.'"
+alias tozsh="chsh -s /bin/zsh && echo ''Now log out.'"
