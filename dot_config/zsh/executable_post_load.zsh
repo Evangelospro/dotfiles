cf6d7eb5e7a25ea888e1a75819ec1a09dd293af3
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-09-12 18:30:43 +0300

    manual zocxide laod

--- dot_config/zsh/executable_post_load.zsh
+++ dot_config/zsh/executable_post_load.zsh
@@ -11,8 +11,14 @@ zicdreplay -q # cache completions for faster startup
 ## FZF tab completion
 export FZF_DEFAULT_COMMAND="fd --hidden --follow --exclude .git || git ls-tree -r --name-only HEAD || rg --files --hidden --follow --glob '!.git' || find ."
 export FZF_DEFAULT_OPTS='-i'
+
+# plugins postload
 # zinit ice wait lucid nocd blockf depth"1"
 # zinit light Aloxaf/fzf-tab
+
+# zinit ice wait"0" lucid nocd has'zoxide'
+# zinit light z-shell/zsh-zoxide
+smartcache eval zoxide init zsh
 
 # end profiler check that it is not already running
 if [[ "${SHELL_PROFILE}" == 1 ]]; then
