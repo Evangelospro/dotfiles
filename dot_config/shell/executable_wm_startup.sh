849a08dd25cb56e28db9e395428eda26929342ad
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-11-17 16:53:43 +0200

    fully switch to uwsm

--- dot_config/shell/executable_wm_startup.sh
+++ dot_config/shell/executable_wm_startup.sh
@@ -1,11 +1,3 @@
-if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
-    if [ -f "$HOME/.config/systemd/user/hyprland-session.service" ]; then
-        exec ~/.config/hypr/start
-    else
-        echo "Error: Hyprland start service not found"
-    fi
+if uwsm check may-start && uwsm select; then
+	systemd-cat -t uwsm_start uwsm start default
 fi
-
-# if uwsm check may-start && uwsm select; then
-# 	systemd-cat -t uwsm_start uwsm start default
-# fi
