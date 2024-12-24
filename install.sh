d150aa4ba098142129f0830ea767b71fa7174f3b
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-12-24 11:26:43 +0200

    organization and cleanup

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 if ! pacman -Qs chezmoi >/dev/null 2>&1; then
-    pacman -S --noconfirm chezmoi
+    sudo pacman -S --noconfirm chezmoi
 fi
 
 chezmoi init && chezmoi apply
 
+if ! pacman -Qs rebos >/dev/null 2>&1; then
+    sudo pacman -S --noconfirm rebos
+fi
 rm -rf ~/.rebos-base
 rebos setup
 rebos gen commit "Install sync"
