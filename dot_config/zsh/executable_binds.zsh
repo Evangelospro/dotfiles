72955ec03680bb155d5ce1376fb3bfd8086f8b8d
Evangelos Lioudakis <elioudakis123@gmail.com>
2025-03-14 07:56:01 +0200

    fix autocomplete binds for zsh

--- dot_config/zsh/executable_binds.zsh
+++ dot_config/zsh/executable_binds.zsh
@@ -1,16 +1,27 @@
-# bindkey -e
+typeset -g -A key
+
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
+key[Insert]="${terminfo[kich1]}"
+key[Backspace]="${terminfo[kbs]}"
+key[Delete]="${terminfo[kdch1]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+key[Left]="${terminfo[kcub1]}"
+key[Right]="${terminfo[kcuf1]}"
+key[PageUp]="${terminfo[kpp]}"
+key[PageDown]="${terminfo[knp]}"
 
 # ZSH autocomplete
 bindkey '^I' insert-unambiguous-or-complete
 bindkey -M menuselect '^I' menu-complete
-bindkey -M menuselect "${terminfo[kcbt]}" reverse-menu-complete
 bindkey -M menuselect '^M' .accept-line
 ## Allow for normal editing keys in the select menu
-bindkey -M menuselect "${terminfo[kcub1]}" .backward-char
-bindkey -M menuselect "${terminfo[kcuf1]}" .forward-char
+bindkey -M menuselect '^[[D' .backward-char '^[OD' .backward-char
+bindkey -M menuselect '^[[C' .forward-char '^[OC' .forward-char
 
 # delete key
-bindkey "${terminfo[kdch1]}" delete-char
+bindkey -- "${key[Delete]}" delete-char
 # bindkey -M menuselect "${terminfo[kdch1]}" delete-char
 # backspace key
 bindkey '^?' backward-delete-char
