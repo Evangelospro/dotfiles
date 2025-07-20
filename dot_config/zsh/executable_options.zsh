ac81277c4524f467ce06dd1586da0b3acec838ca
Evangelos Lioudakis <elioudakis123@gmail.com>
2025-07-20 17:23:51 +0300

    fix ruby home

--- dot_config/zsh/executable_options.zsh
+++ dot_config/zsh/executable_options.zsh
@@ -1,8 +1,11 @@
-# setopt autocd        # if only directory path is entered, cd there.
+setopt autocd        # if only directory path is entered, cd there.
 setopt auto_pushd
 setopt pushd_ignore_dups
-setopt pushdminus
+setopt pushd_minus
 setopt complete_in_word
 setopt always_to_end
 
-setopt INTERACTIVE_COMMENTS
+setopt interactive_comments
+setopt extended_glob
+setopt long_list_jobs
+setopt null_glob
