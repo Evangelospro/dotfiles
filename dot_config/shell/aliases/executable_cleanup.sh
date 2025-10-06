774208b6f067f909c872730de643661544e7f235
Evangelos Lioudakis <elioudakis123@gmail.com>
2025-10-06 21:54:33 +0200

    update flameshot config

--- dot_config/shell/aliases/executable_cleanup.sh
+++ dot_config/shell/aliases/executable_cleanup.sh
@@ -2,7 +2,7 @@
 alias clean-cache="/usr/bin/rm -rf $HOME/.cache/* && _evalcache_clear" # shows no mercy but it is cache right?
 alias clean-vscode-cache="/usr/bin/rm -rf $HOME/.config/Code*/Cache/* && /usr/bin/rm -rf $HOME/.config/Code*/CachedData/*"
 alias clean-docker='docker system prune -af --volumes --filter="label!=DO_NOT_PRUNE=true"'
-alias clean-packages='sudo pacman -Rns $(pacman -Qtdq) && sudo /usr/bin/rm -rf /var/cache/pacman/pkg/*'
+alias clean-packages='sudo pacman -Rns $(pacman -Qtdq) && sudo /usr/bin/rm -rf /var/cache/pacman/pkg/* && sudo rm -rf /tmp/makepkg/*'
 alias clean-font-cache="fc-cache -f -v"
 alias clean-seafile-cache="/usr/bin/rm -rf $HOME/.seadrive/data/file-cache"
 alias clear-trash="/usr/bin/rm -rf $HOME/.local/share/Trash/*"
