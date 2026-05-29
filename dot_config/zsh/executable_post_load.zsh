1c180d25ac8ab5018bfae86f82396e50384d4bd4
Evangelos Lioudakis <elioudakis123@gmail.com>
2026-05-29 18:52:48 +0300

    fix minor zsh errors

--- dot_config/zsh/executable_post_load.zsh
+++ dot_config/zsh/executable_post_load.zsh
@@ -8,14 +8,6 @@ autoload -Uz _zinit
 zicompinit
 zicdreplay -q # cache completions for faster startup
 
-## FZF tab completion
-export FZF_DEFAULT_COMMAND="fd --hidden --follow --exclude .git || git ls-tree -r --name-only HEAD || rg --files --hidden --follow --glob '!.git' || find ."
-export FZF_DEFAULT_OPTS='-i'
-
-# plugins postload
-# zinit ice wait lucid nocd blockf depth"1"
-# zinit light Aloxaf/fzf-tab
-
 # zinit ice wait"0" lucid nocd has'zoxide'
 # zinit light z-shell/zsh-zoxide
 smartcache eval zoxide init zsh
--- dot_config/zsh/executable_zstyle.zsh
+++ dot_config/zsh/executable_zstyle.zsh
@@ -1,9 +1,9 @@
 # Debugging
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*' debug yes
+# zstyle ':completion:*' verbose yes
+# zstyle ':completion:*' debug yes
 
 # Speed up completions
-zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' accept-exact yes
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 zstyle ':completion:*' rehash true
@@ -24,7 +24,13 @@ zstyle ':autocomplete:*' fzf-completion yes
 zstyle ':autocomplete:*' widget-style menu-select
 zstyle ':autocomplete:*' add-space '' # never add space after completion
 
-zstyle ':completion:*' completer _expand_alias _extensions _complete _approximate
+zstyle -e ':completion:*' completer '
+	if [[ $_last_try != "$HISTNO$BUFFER$CURSOR" ]]; then
+		_last_try="$HISTNO$BUFFER$CURSOR"
+		reply=(_extensions _complete _prefix)
+	else
+		reply=(_ignored _correct _approximate)
+	fi'
 zstyle ':completion:*:paths' path-completion yes
 zstyle ':completion:*:matches' group 'yes'
 zstyle ':completion:*' group-name ''
@@ -39,40 +45,6 @@ zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
 zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-
-# Fzf tab complete
-# zstyle ':fzf-tab:*' switch-group '<' '>'
-# zstyle ':fzf-tab:*' continuous-trigger ''
-# zstyle ':fzf-tab:complete:(cd|ls|cat|nano|vi|vim|__zoxide_z):*' fzf-preview 'lsd -1 $realpath'
-# zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
-# 	fzf-preview 'echo ${(P)word}'
-## Preview `kill` and `ps` commands
-# zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview \
-#         '[[ $group == "[process ID]" ]] &&
-#             if [[ $OSTYPE == darwin* ]]; then
-#             ps -p $word -o comm="" -w -w
-#             elif [[ $OSTYPE == linux* ]]; then
-#             ps --pid=$word -o cmd --no-headers -w -w
-#             fi'
-# zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
-## Preview `git` commands
-zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview \
-	'git diff $word | delta'
-zstyle ':fzf-tab:complete:git-log:*' fzf-preview \
-	'git log --color=always $word'
-zstyle ':fzf-tab:complete:git-help:*' fzf-preview \
-	'git help $word | bat -plman --color=always'
-zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
-	'case "$group" in
-	"commit tag") git show --color=always $word ;;
-	*) git show --color=always $word | delta ;;
-	esac'
-zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
-	'case "$group" in
-	"modified file") git diff $word | delta ;;
-	"recent commit object name") git show --color=always $word | delta ;;
-	*) git log --color=always $word ;;
-	esac'
 
 # docker
 zstyle ':completion:*:*:docker:*' option-stacking yes
