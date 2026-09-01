8b3c36303c55f3048e5cd18b8ccbcc7b017846e3
Evangelos Lioudakis <elioudakis123@gmail.com>
2026-09-01 04:55:00 +0300

    add hyprexpo plugin

---
+++ dot_config/hypr/plugins.lua
@@ -0,0 +1,14 @@
+hl.config({
+    plugin = {
+        hyprexpo = {
+            columns = 3,
+            gaps_in = 5,
+            gaps_out = 0,
+            bg_col = "rgb(111111)",
+            workspace_method = "center current",
+            gesture_distance = 200,
+            cancel_key = "escape",
+            show_cursor = 1,
+        },
+    },
+})
