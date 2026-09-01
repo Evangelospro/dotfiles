44d9ce6c4a036261d276c7483032d0c3bea5a1ed
Evangelos Lioudakis <elioudakis123@gmail.com>
2026-09-01 04:55:00 +0300

    old files cleanup

--- dot_config/hypr/plugins.lua
+++ dot_config/hypr/plugins.lua
@@ -1,14 +1,14 @@
 hl.config({
-    plugin = {
-        hyprexpo = {
-            columns = 3,
-            gaps_in = 5,
-            gaps_out = 0,
-            bg_col = "rgb(111111)",
-            workspace_method = "center current",
-            gesture_distance = 200,
-            cancel_key = "escape",
-            show_cursor = 1,
-        },
-    },
+    -- plugin = {
+    --     hyprexpo = {
+    --         columns = 3,
+    --         gaps_in = 5,
+    --         gaps_out = 0,
+    --         bg_col = "rgb(111111)",
+    --         workspace_method = "center current",
+    --         gesture_distance = 200,
+    --         cancel_key = "escape",
+    --         show_cursor = 1,
+    --     },
+    -- },
 })
