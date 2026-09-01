fecb03568f3a797457f1fc580aedcee0942a9524
Evangelos Lioudakis <elioudakis123@gmail.com>
2026-09-01 04:55:00 +0300

    remove elephant and onlyoffice

--- dot_config/exact_shell/aliases/executable_hacking.sh
+++ dot_config/exact_shell/aliases/executable_hacking.sh
@@ -1,8 +1,4 @@
 # HACKING TOOLS
-## Listeners
-alias penelope="penelope --configfile $HOME/.config/penelope/penelope.conf"
-## Active Directory
-alias cme='crackmapexec'
 ## Stego
 alias stego-toolkit='docker run -it --rm -v $(pwd)/data:/data dominicbreuker/stego-toolkit /bin/bash'
 ## Mobile
