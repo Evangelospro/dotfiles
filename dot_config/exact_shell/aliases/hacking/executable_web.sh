c244d65b2ea3bad2dea32ec07d870bf65044c35f
Evangelos Lioudakis <elioudakis123@gmail.com>
2026-09-01 04:55:00 +0300

    Remove old configs

--- dot_config/exact_shell/aliases/hacking/executable_web.sh
+++ dot_config/exact_shell/aliases/hacking/executable_web.sh
@@ -4,8 +4,6 @@ if [ -f /opt/sqlmap-bin/sqlmap.py ]; then
 elif [ -f /opt/sqlmap/sqlmap.py ]; then
     alias sqlmap="/opt/sqlmap/sqlmap.py"
 fi
-alias sstimap="python3 $HACKING_TOOLS/Web/SSTImap/sstimap.py"
 alias wpscan='wpscan -e ap,t,u --plugins-detection aggressive'
-alias jwt-tool="python3 $HACKING_TOOLS/Web/jwt_tool/jwt_tool.py"
-alias webserver='updog -p 8000'
-alias ftpup='sudo python -m pyftpdlib -p 21'
+alias web-server='updog -p 8000'
+alias ftp-server='sudo python -m pyftpdlib -p 21'
